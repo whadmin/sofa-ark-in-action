9d01890d343e7d962551e2fe23c99ff6dde7cb1c
wuhao10 <wuhao10@xiaomi.com>
2020-09-10 17:19:46 +0800

    first commit

---
+++ service-v2/src/main/java/com/github/demo/MyPluginActivatorV2.java
@@ -0,0 +1,25 @@
+package com.github.demo;
+
+
+import com.alipay.sofa.ark.exception.ArkRuntimeException;
+import com.alipay.sofa.ark.spi.model.PluginContext;
+import com.alipay.sofa.ark.spi.service.PluginActivator;
+
+/**
+ * A sample ark-plugin activator
+ *
+ * @author qilong.zql
+ * @since 0.1.0
+ */
+public class MyPluginActivatorV2 implements PluginActivator{
+
+    public void start(PluginContext context) throws ArkRuntimeException {
+        System.out.println("starting in sample ark plugin activator");
+        context.publishService(MyServiceV2.class, new MyServiceV2());
+    }
+
+    public void stop(PluginContext context) throws ArkRuntimeException {
+        System.out.println("stopping in ark plugin activator");
+    }
+
+}
